27ca2d50290d28d38c400c01b34febb64dcb78ed
Vishweshwarayya Kj <k.vishu@outlook.com>
2023-10-07 11:40:18 +0530

    Caseworker: added frontend and backend folders

---
+++ cw_backend/src/main/java/com/master/cw_backend/CwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.master.cw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwBackendApplication.class, args);
+	}
+
+}
---
+++ cw_backend/src/test/java/com/master/cw_backend/CwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.cw_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
